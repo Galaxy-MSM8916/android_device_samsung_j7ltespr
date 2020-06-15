f5b62d09370810d9a8ff947c6ea07d4eba11a5b6
Dan Willemsen <dwillemsen@google.com>
2020-06-15 19:53:02 +0800

    j7ltespr: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: I34a99b98f5dbfbd51dcbb7a9f83006e0d63947e4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,3 +4,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_j7ltespr.mk \
 	$(LOCAL_DIR)/pa_j7ltespr.mk \
 	$(LOCAL_DIR)/lineage_j7ltespr.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j7ltespr-eng \
+    lineage_j7ltespr-userdebug \
+    lineage_j7ltespr-user
